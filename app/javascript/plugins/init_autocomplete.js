2f84387aeac45c81425d1bd17fa105ad6cfb9b10
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-25 18:06:44 +0200

    home page view

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,7 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('flat_address');
+  const addressInput = document.getElementById('shop_address');
   if (addressInput) {
     places({ container: addressInput });
   }
