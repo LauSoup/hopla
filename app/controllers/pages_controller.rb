fba572ff57c7b9bbde7338da894c31ff9964fe4c
Edgar Minault <e.minault@bluewin.ch>
2020-05-25 16:53:21 +0200

    Dashboard method defined in pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,17 @@ class PagesController < ApplicationController
   def home
     @shops = Shop.geocoded
     @markers = @shops.map do |shop|
-          {
-            lat: shop.latitude,
-            lng: shop.longitude
-          }
-        end
+      {
+        lat: shop.latitude,
+        lng: shop.longitude
+      }
+    end
   end
+
+  def dashboard
+    @user = current_user
+    @shops = Shop.all.select{|shop| shop.user_id == @user.id}
+  end
+
+
 end
