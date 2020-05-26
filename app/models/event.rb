8a611fba8f2a3f61149a84ba60fbc0a11d3c2d25
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-26 12:05:07 +0200

    update & create event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,17 @@
 class Event < ApplicationRecord
   belongs_to :shop
-
   has_many :favorites_events
   has_many :users, through: :favorites_events
+
+  CATEGORIES = ["Evènement", "Promotion"]
   validates :title, :beg_date, :end_date, :category, presence: true
   validates :offer, presence: true, if: -> {
     category == "Promotion"
   }
-  validates :category, inclusion: { in: %w(Evènement Promotion) }
+  validates :category, inclusion: { in: CATEGORIES }
   # has_one_attached :photo
   
-    def self.random
+  def self.random
     @events = Event.all
     @events.sample
   end
