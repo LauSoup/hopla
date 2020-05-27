1d8685bb62539128e60bc2e2902e17745cb5427d
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-27 15:33:02 +0200

    display map nicely

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,33 +6,9 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v10'
   });
 };
-
-const AddSearchToMap = (map) => {
-  map.addControl(
-    new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl })
-  );
-};
-
-// const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-//     const element = document.createElement('div');
-//     element.className = 'marker';
-//     element.style.backgroundImage = `url('${marker.image_url}')`;
-//     element.style.backgroundSize = 'cover';
-//     element.style.width = '20px';
-//     element.style.height = '30px';
-//     new mapboxgl.Marker(element)
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .setPopup(popup)
-//       .addTo(map);
-//   });
-// };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -51,75 +27,9 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
-      map.addControl(
-      new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl })
-    );
-    addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
-    AddSearchToMap(map);
+
   }
 };
 
 export { initMapbox };
-
-
-// const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-//     const element = document.createElement('div');
-//     element.className = 'marker';
-//     element.style.backgroundImage = `url('${marker.image_url}')`;
-//     element.style.backgroundSize = 'cover';
-//     element.style.width = '20px';
-//     element.style.height = '30px';
-//     new mapboxgl.Marker(element)
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .setPopup(popup)
-//       .addTo(map);
-//   });
-// };
-
-
-// const initMapbox = () => {
-//   const mapElement = document.getElementById('map');
-
-//   if (mapElement) { // only build a map if there's a div#map to inject into
-//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v11'
-//     });
-
-//     const markers = JSON.parse(mapElement.dataset.markers);
-//   markers.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(map);
-//   });
-//   }
-//   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//   const map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11',
-//   center: [-1.6777926, 48.117266],
-//   zoom: 13
-//   });
-
-//   map.addControl(
-//     new MapboxGeocoder({
-//       accessToken: mapboxgl.accessToken,
-//       mapboxgl: mapboxgl })
-//   );
-//   addMarkersToMap(map, markers);
-//   fitMapToMarkers(map, markers);
-// };
-
-// const fitMapToMarkers = (map, markers) => {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-// };
-
-// export { initMapbox };
