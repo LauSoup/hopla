7d926174dd8efd5d8cf703174a0c208c72536aec
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-27 17:10:52 +0200

    carousel dependence

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts = nil
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
