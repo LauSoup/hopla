8d957af6be96f6a3df5754fff7d5bee5a486b7b0
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-27 15:07:28 +0200

    correct js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+//= require jquery
+//= require jquery_ujs
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
