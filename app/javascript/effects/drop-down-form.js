35f28b375603cb6afbcfdecf41664d66d1ed955b
Edgar Minault <e.minault@bluewin.ch>
2020-05-28 16:51:37 +0200

    Home page containers fixed.

---
+++ app/javascript/effects/drop-down-form.js
@@ -0,0 +1,14 @@
+const dropdownForm = () => {
+  const dropdownButton = document.getElementById("refine-button");
+  console.log(dropdownButton);
+
+  dropdownButton.addEventListener("click", (event) => {
+    console.log(event);
+    const form = document.getElementById("search-form-home");
+    form.classList.toggle("d-none");
+
+  });
+
+};
+
+export { dropdownForm }
