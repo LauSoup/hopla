6175e71da396a9293b828ba0dbfc9b095707ad0e
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-28 17:43:12 +0200

    debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,7 +37,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
-  dropdownForm();
+  // dropdownForm();
   // rippleButtons();
   flatpickr(".datepicker");
 });
