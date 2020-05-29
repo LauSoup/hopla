af9af7b16c1499127470302d908f7a88fcb536c4
Edgar Minault <e.minault@bluewin.ch>
2020-05-29 15:24:02 +0200

    Change dropdown chevron.

--- app/javascript/effects/dropdown_form.js
+++ app/javascript/effects/dropdown_form.js
@@ -1,9 +1,15 @@
 const dropdownForm = () => {
   const dropdownButton = document.getElementById("refine-button");
-  if (dropdownButton) { 
+  if (dropdownButton) {
+    console.log(dropdownButton);
     dropdownButton.addEventListener("click", (event) => {
       const form = document.getElementById("search-form-home");
       form.classList.toggle("d-none");
+      if (dropdownButton.innerHTML === "<i class=\"fas fa-chevron-down\"></i> Filtrer") {
+        dropdownButton.innerHTML = "<i class=\"fas fa-chevron-up\"></i> Filtrer";
+      } else {
+        dropdownButton.innerHTML = "<i class=\"fas fa-chevron-down\"></i> Filtrer";
+      }
     });
   };
 };
