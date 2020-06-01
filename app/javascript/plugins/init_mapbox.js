d9be9fb795d0cb77117978c646fe5b2ee2941f2f
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-06-01 12:55:20 +0200

    new display windowpopup

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,7 +21,7 @@ const addMarkersToMap = (map, markers) => {
     element.style.height = '50px';
     new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // add this
+      .setPopup(popup)
       .addTo(map);
   });
 };
@@ -29,7 +29,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 50, maxZoom: 15 });
 };
 
 const initMapbox = () => {
