551305c18f016b0bc5714404b838633d43af1160
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-06-18 17:00:38 +0200

    display message different if user is shop owner or not

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,6 @@ puts "✨✨ Hoplette created !! ✨✨"
 
 puts "⏲ Initializing categories... ⏲"
 
-
 categories = [
   divertissement = { name: "Divertissement", icon: "fas fa-theater-masks" },
   electronique = { name: "Electronique", icon: "fas fa-tv" },
@@ -65,165 +64,165 @@ puts "✨✨ Categories created !! ✨✨"
 
 # create 5 users with 1 shop, 1 event by shop
 
-5.times do
+# 5.times do
 
-  # Gathering RANDOMUSER API to create users and shops
-  # Opening and parsing JSON.
-  data_url = "https://randomuser.me/api/?nat=fr"
-  random_user_json = open(data_url).read
-  random_user = JSON.parse(random_user_json)
+#   # Gathering RANDOMUSER API to create users and shops
+#   # Opening and parsing JSON.
+#   data_url = "https://randomuser.me/api/?nat=fr"
+#   random_user_json = open(data_url).read
+#   random_user = JSON.parse(random_user_json)
 
-  puts "🙋 Creating user..."
-  first_name = random_user["results"][0]["name"]["first"]
-  last_name = random_user["results"][0]["name"]["last"]
+#   puts "🙋 Creating user..."
+#   first_name = random_user["results"][0]["name"]["first"]
+#   last_name = random_user["results"][0]["name"]["last"]
 
-  email = "#{first_name.downcase}.#{last_name.downcase}@hoplette.com"
-  password = "hoplette"
-  user = User.new(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    password: password
-  )
-  if user.valid?
-    user.save!
-    puts "User #{user.first_name} #{user.last_name} created ! 🙌"
-  else
-    puts "User not created due to duplicate data 😢"
-  end
+#   email = "#{first_name.downcase}.#{last_name.downcase}@hoplette.com"
+#   password = "hoplette"
+#   user = User.new(
+#     first_name: first_name,
+#     last_name: last_name,
+#     email: email,
+#     password: password
+#   )
+#   if user.valid?
+#     user.save!
+#     puts "User #{user.first_name} #{user.last_name} created ! 🙌"
+#   else
+#     puts "User not created due to duplicate data 😢"
+#   end
 
 
-  #creating shop
-  if user.valid?
-    puts "🙋 Creating shop for this user..."
-    active = true
-    name = Faker::TvShows::Simpsons.location
+#   #creating shop
+#   if user.valid?
+#     puts "🙋 Creating shop for this user..."
+#     active = true
+#     name = Faker::TvShows::Simpsons.location
 
-    # Gathering elements of the address and create the full address
-    address = ["9 rue bouquière, bordeaux", "22 quai duguay trouin, rennes", "9 rue du chemin vert, paris", "6 rue pizay, lyon", "4 rue des cigognes, strasbourg", "10 rue du guesclin, marseille"].sample
-    # street_nb = (2..12).to_a.sample
-    # street = random_user["results"][0]["location"]["street"]["name"]
-    # # cp = random_user["results"][0]["location"]["postcode"]
-    # city = random_user["results"][0]["location"]["city"]
-    # address = "#{street_nb} #{street}, #{city}"
+#     # Gathering elements of the address and create the full address
+#     address = ["9 rue bouquière, bordeaux", "22 quai duguay trouin, rennes", "9 rue du chemin vert, paris", "6 rue pizay, lyon", "4 rue des cigognes, strasbourg", "10 rue du guesclin, marseille"].sample
+#     # street_nb = (2..12).to_a.sample
+#     # street = random_user["results"][0]["location"]["street"]["name"]
+#     # # cp = random_user["results"][0]["location"]["postcode"]
+#     # city = random_user["results"][0]["location"]["city"]
+#     # address = "#{street_nb} #{street}, #{city}"
 
-    # Finish shop creation
+#     # Finish shop creation
 
-    description = Faker::ChuckNorris.fact
+#     description = Faker::ChuckNorris.fact
 
-    shop = Shop.new(
-      active: active,
-      name: name,
-      address: address,
-      description: description
-    )
-    shop.user_id = user.id
-    shop.save!
+#     shop = Shop.new(
+#       active: active,
+#       name: name,
+#       address: address,
+#       description: description
+#     )
+#     shop.user_id = user.id
+#     shop.save!
 
-    if shop.valid?
-      shop.save!
-      puts  "Shop #{shop.name} saved 🙌"
-    else
-      puts "Shop not created due to duplicate data 😢"
-    end
+#     if shop.valid?
+#       shop.save!
+#       puts  "Shop #{shop.name} saved 🙌"
+#     else
+#       puts "Shop not created due to duplicate data 😢"
+#     end
 
-    # Add category (tags) to the shop (created a class method random in category)
+#     # Add category (tags) to the shop (created a class method random in category)
 
-    puts "🏠 Creating 1, 2 or 3 categories (tags) for this shop..."
+#     puts "🏠 Creating 1, 2 or 3 categories (tags) for this shop..."
 
-    i = (1..3).to_a.sample
+#     i = (1..3).to_a.sample
 
-    i.times do
-      category = Category.random
-      tag = Tag.new
-      tag.category_id = category.id
-      tag.shop_id = shop.id
-      tag.save!
-    end
+#     i.times do
+#       category = Category.random
+#       tag = Tag.new
+#       tag.category_id = category.id
+#       tag.shop_id = shop.id
+#       tag.save!
+#     end
 
-    puts "#{i} tag(s) added ⛄"
+#     puts "#{i} tag(s) added ⛄"
 
-    # Add events to the shop
+#     # Add events to the shop
 
-    puts "📅 Creating 1, 2 or 3 events for this shop... "
+#     puts "📅 Creating 1, 2 or 3 events for this shop... "
 
-    i = (1..3).to_a.sample
+#     i = (1..3).to_a.sample
 
-    i.times do
-      title = ["10% de réduction sur tout !", "Vernissage exceptionnel 🎇", "Un thé offert pour toute visite 🍵", "Nouvelle collection dans les rayons", "Réductions jusqu'à 30% pour fin de collection!"].sample
-      description = ["On vous attend nombreux!", "N'oubliez pas votre tot-bag pour limiter les sacs jetables 😉 "].sample
-      beg_date = Faker::Date.between(from: 2.days.ago, to: Date.today)
-      end_date = Faker::Date.between(from: Date.today, to: 20.days.from_now)
-      category = ["Evènement", "Promotion"].sample
-      offer = ["10%", "2 achetés 1 offert", "Un thé offert"].sample
-      active = [true, false].sample
-      buyers = (50..100).to_a.sample
-      event = Event.new(
-        title: title,
-        description: description,
-        offer: offer,
-        beg_date: beg_date,
-        end_date: end_date,
-        category: category,
-        active: active,
-        buyers: buyers,
-        qr_code: nil
-      )
-      event.shop_id = shop.id
-      event.save!
-      qr_code = "www.hop-hop-hop.com/events/#{event.id}"
-      event.qr_code = qr_code
-      event.save!
-    end
+#     i.times do
+#       title = ["10% de réduction sur tout !", "Vernissage exceptionnel 🎇", "Un thé offert pour toute visite 🍵", "Nouvelle collection dans les rayons", "Réductions jusqu'à 30% pour fin de collection!"].sample
+#       description = ["On vous attend nombreux!", "N'oubliez pas votre tot-bag pour limiter les sacs jetables 😉 "].sample
+#       beg_date = Faker::Date.between(from: 2.days.ago, to: Date.today)
+#       end_date = Faker::Date.between(from: Date.today, to: 20.days.from_now)
+#       category = ["Evènement", "Promotion"].sample
+#       offer = ["10%", "2 achetés 1 offert", "Un thé offert"].sample
+#       active = [true, false].sample
+#       buyers = (50..100).to_a.sample
+#       event = Event.new(
+#         title: title,
+#         description: description,
+#         offer: offer,
+#         beg_date: beg_date,
+#         end_date: end_date,
+#         category: category,
+#         active: active,
+#         buyers: buyers,
+#         qr_code: nil
+#       )
+#       event.shop_id = shop.id
+#       event.save!
+#       qr_code = "www.hop-hop-hop.com/events/#{event.id}"
+#       event.qr_code = qr_code
+#       event.save!
+#     end
 
-    puts "#{i} event(s) added 📆"
-  end
-end
+#     puts "#{i} event(s) added 📆"
+#   end
+# end
 
-# Create users without shops
+# # Create users without shops
 
-5.times do
-  puts "🙋 Creating user without shop..."
+# 5.times do
+#   puts "🙋 Creating user without shop..."
 
-  # Gathering RANDOMUSER API to create users and shops
-  # Opening and parsing JSON.
-  data_url = "https://randomuser.me/api/?nat=fr"
-  random_user_json = open(data_url).read
-  random_user = JSON.parse(random_user_json)
+#   # Gathering RANDOMUSER API to create users and shops
+#   # Opening and parsing JSON.
+#   data_url = "https://randomuser.me/api/?nat=fr"
+#   random_user_json = open(data_url).read
+#   random_user = JSON.parse(random_user_json)
 
-  first_name = random_user["results"][0]["name"]["first"]
-  last_name = "No_shop"
-  email = "#{first_name.downcase}.#{last_name.downcase}@hoplette.com"
-  password = "hoplette"
-  user = User.new(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    password: password
-  )
+#   first_name = random_user["results"][0]["name"]["first"]
+#   last_name = "No_shop"
+#   email = "#{first_name.downcase}.#{last_name.downcase}@hoplette.com"
+#   password = "hoplette"
+#   user = User.new(
+#     first_name: first_name,
+#     last_name: last_name,
+#     email: email,
+#     password: password
+#   )
 
-  if user.valid?
-    user.save!
-    puts "User #{user.first_name} #{user.last_name} created ! 🙌"
-  else
-    puts "User not created due to duplicate data 😢"
-  end
-end
+#   if user.valid?
+#     user.save!
+#     puts "User #{user.first_name} #{user.last_name} created ! 🙌"
+#   else
+#     puts "User not created due to duplicate data 😢"
+#   end
+# end
 
-# Create random favories on random users
+# # Create random favories on random users
 
-puts "Putting 20 events as favorites for random users..."
+# puts "Putting 20 events as favorites for random users..."
 
-20.times do
-  user = User.random
-  event = Event.random
-  favorite_event = FavoriteEvent.new
-  favorite_event.user_id = user.id
-  favorite_event.event_id = event.id
-  favorite_event.save!
-end
+# 20.times do
+#   user = User.random
+#   event = Event.random
+#   favorite_event = FavoriteEvent.new
+#   favorite_event.user_id = user.id
+#   favorite_event.event_id = event.id
+#   favorite_event.save!
+# end
 
-puts "Finishing creation of 20 events as favorites 🎆"
+# puts "Finishing creation of 20 events as favorites 🎆"
 
 
 # Creating user who has every shops
@@ -247,8 +246,6 @@ puts "✨✨ Admin created !! ✨✨"
 
 puts "Creating real shops"
 
-
-
 file = 'db/shops.json'
 
 shops = JSON.parse(File.read(file))
@@ -256,12 +253,11 @@ shops["shops"].each do |shop|
   shop = Shop.new(
     active: true,
     name:  shop["name"],
-    address: shop["address"]
+    address: shop["address"],
+    description: shop["description"]
   )
   puts "#{shop[:name]} created based on #{shop[:address]}"
   shop.user_id = user1.id
   shop.save! if shop.valid?
 end
-
-puts "Ending creating real shops"
-
+  puts "Ending creating real shops"
